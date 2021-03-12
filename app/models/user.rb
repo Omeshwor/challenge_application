1bb99cca4cb1f83ef7433211642cc72a79d4030c
Omeshwor <omeshworlalshrestha@yahoo.com>
2021-03-11 18:21:43 -0600

    Generate devise views and controllers and add user views index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
-  has_many:challenges, dependent: :destroy
-  has_many: friendships, dependent: :destroy
-  has_many: friends, through: :friendship, dependent: :destroy
+  
+  has_many :challenges, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendship, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "challenges#index"
   resources :challenges
   devise_for :users
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
