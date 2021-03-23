955b2520b69382d908738c9083dc0cab13998d31
Omeshwor <omeshworlalshrestha@yahoo.com>
2021-03-23 01:01:16 -0500

    Make changes to schemas for heroku

--- db/migrate/20210317190916_create_competitors.rb
+++ db/migrate/20210317190916_create_competitors.rb
@@ -1,7 +1,7 @@
-class Competitors < ActiveRecord::Migration[6.0]
+class CreateCompetitors < ActiveRecord::Migration[6.0]
   def change
     create_table :competitors do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: {to_table: :users }
       t.references :challenge, null: false, foreign_key: true
       t.timestamps
     end
--- db/migrate/20210318204451_alter_competitors_user.rb
+++
@@ -1,7 +0,0 @@
-class AlterCompetitorsUser < ActiveRecord::Migration[6.0]
-  def change
-    change_table :friendships do |t|
-      t.references :competitor, references: :users, foreign_key: {to_table: :users}
-    end
-  end
-end
